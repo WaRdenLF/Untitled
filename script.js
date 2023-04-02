adb9ed88a2d5794edd67d1a92241ac7184588774
diyorbekjoraqulov8 <you@gmail.com>
2023-04-02 18:11:09 +0500

    oybek

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let offset = 0
-const sliderLine = document.querySelector ('.card__menu')
+const sliderLine = document.querySelectorAll('.cards__one')
 
-document.querySelector('.wrapper__btn').addEventListener('click' , function() {
+
+document.querySelector('#prev-btn').addEventListener('click', function () {
     offset = offset + 256
     if (offset > 768) {
         offset = 0
-        
+
     }
-    sliderLine.style.left = -offset + 'px'
+
+
+ 
 })
-
-
